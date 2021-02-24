dcc231aac46466e5b01c5ec345f6ae438d5ff3d3
PeterhuzzaJPS <71355611+PeterhuzzaJPS@users.noreply.github.com>
2021-02-24 13:25:58 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey Ya'll. Whats Up?'));
+app.get('/', (req, res) => res.send('Learnin AWS stuffs'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
